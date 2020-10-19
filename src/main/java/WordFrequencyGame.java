d8240712fa7f6043f98ea4c0f15455b8b936b7b9
dominje <jericah.domingo@oocl.com>
2020-10-19 16:07:15 +0800

    Revert "[word-frequency][jericah]refactor: extracted to a method"
    This reverts commit 9a750dcf

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -13,7 +13,22 @@ public class WordFrequencyGame {
 
             try {
 
-                List<WordInfo> wordInfoList = calculateWordInfo(sentence);
+                String[] words = sentence.split(WHITE_SPACES);
+
+                List<WordInfo> wordInfoList = new ArrayList<>();
+                for (String word : words) {
+                    WordInfo wordInfo = new WordInfo(word, 1);
+                    wordInfoList.add(wordInfo);
+                }
+
+                Map<String, List<WordInfo>> wordInfoMap =getListMap(wordInfoList);
+
+                List<WordInfo> distinctWordInfoList = new ArrayList<>();
+                for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()){
+                    WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
+                    distinctWordInfoList.add(wordInfo);
+                }
+                wordInfoList = distinctWordInfoList;
 
                 wordInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
 
@@ -29,26 +44,6 @@ public class WordFrequencyGame {
                 return "Calculate Error";
             }
         }
-    }
-
-    private List<WordInfo> calculateWordInfo(String sentence) {
-        String[] words = sentence.split(WHITE_SPACES);
-
-        List<WordInfo> wordInfoList = new ArrayList<>();
-        for (String word : words) {
-            WordInfo wordInfo = new WordInfo(word, 1);
-            wordInfoList.add(wordInfo);
-        }
-
-        Map<String, List<WordInfo>> wordInfoMap =getListMap(wordInfoList);
-
-        List<WordInfo> distinctWordInfoList = new ArrayList<>();
-        for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()){
-            WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-            distinctWordInfoList.add(wordInfo);
-        }
-        wordInfoList = distinctWordInfoList;
-        return wordInfoList;
     }
 
 
